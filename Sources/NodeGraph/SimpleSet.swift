0906f4a19dab265d64ccb843e851284814bdf030
Mikael Sundström <mikael.sundstrom@chas.se>
2020-02-15 16:43:36 +0100

    Fix set class

--- Sources/NodeGraph/SimpleSet.swift
+++ Sources/NodeGraph/SimpleSet.swift
@@ -27,27 +27,42 @@ class SimpleStrongSet<ItemType: Equatable>: SimpleSet {
     }
 }
 
-fileprivate class WeakItemWrapper<ItemType: Equatable, Class> {
+fileprivate class WeakItemWrapper<ItemType: AnyObject & Equatable> {
     weak var item: ItemType?
     init(item: ItemType) {
         self.item = item
     }
 }
 
-class SimpleWeakSet<ItemType: Equatable>: SimpleSet {
-    private var _items = [ItemType]()
+class SimpleWeakSet<ItemType: AnyObject & Equatable>: SimpleSet {
+    private var _items = [WeakItemWrapper<ItemType>]()
     var count: Int {
+        clean()
         return _items.count
     }
     
     func add(item: ItemType) {
-        guard !_items.contains(item) else {
+        clean()
+        guard !(_items.contains { $0.item == item }) else {
             return
         }
-        _items.append(item)
+        _items.append(WeakItemWrapper(item: item))
     }
     
     func remove(item: ItemType) {
-        _items.removeAll { $0 == item }
+        clean()
+        _items.removeAll { $0.item == item }
+    }
+    
+    private func clean() {
+        guard _items.count > 0 else {
+            return
+        }
+        for i in _items.count-1...0 {
+            let wrapper = _items[i]
+            if wrapper.item == nil {
+                _items.remove(at: i)
+            }
+        }
     }
 }
